a1d55756baa663e4a6e20ce11d88e8aa0a3856e8
Bret Andrew Van Hof <bretvh@hotmail.com>
2016-05-27 14:56:01 -0700

    Refactor work
    Also include generated pdfs .exe files....

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -7,40 +7,40 @@ using namespace std;
 
 template <class T>
 class BinarySearchTree : public BinaryTree<T> {
- public:
-  BinarySearchTree() { this->root = NULL; }
-  BinarySearchTree( const BinarySearchTree *src ) : BinaryTree<T>(src) {}
-  BinarySearchTree( T *elements, int n_elements );
- 
-  
-  /* BST Operations */
-  bool insert( const T& elem );
-  bool insert(BTNode<T>*& node, const T);
-  template<class K> T* lookup( const K& key ) const;
-  template<class K> T* lookup( const K& key, BTNode<T>* node ) const;
-  bool empty_this() { empty(BinaryTree<T>::root); BinaryTree<T>::root = NULL; return true; }
+public:
+	BinarySearchTree() { this->root = NULL; }
+	BinarySearchTree(const BinarySearchTree *src) : BinaryTree<T>(src) {}
+	BinarySearchTree(T *elements, int n_elements);
 
 
-  /* Other Operations */
-  void init_from_sorted_array( T *elements, int n_elements );
-  int to_array( T *elements, int max );
-  int to_array(T* elements, int max, BTNode<T>* thisRoot );
-  void init( T* elements, int n_elements );
-  void init_from_sorted_array( T* elements, int start, int end );
+	/* BST Operations */
+	bool insert(const T& elem);
+	bool insert(BTNode<T>*& node, const T);
+	template<class K> T* lookup(const K& key) const;
+	template<class K> T* lookup(const K& key, BTNode<T>* node) const;
+	bool empty_this() { empty(BinaryTree<T>::root); BinaryTree<T>::root = NULL; return true; }
 
-  /* Input/Output Operators */
-  template<class S>
-  friend istream& operator>>( istream& in, BinarySearchTree<S>& obj );
-  
- template<class S>
-  friend ostream& operator<<( ostream& in, BinarySearchTree<S>& obj );
- 
-template<class S>
-  friend ostream& inorderPrint( ostream& out, BTNode<S>* node );
- 
+
+	/* Other Operations */
+	void init_from_sorted_array(T *elements, int n_elements);
+	int to_array(T *elements, int max);
+	int to_array(T* elements, int max, BTNode<T>* thisRoot);
+	void init(T* elements, int n_elements);
+	void init_from_sorted_array(T* elements, int start, int end);
+
+	/* Input/Output Operators */
+	template<class S>
+	friend istream& operator >> (istream& in, BinarySearchTree<S>& obj);
+
+	template<class S>
+	friend ostream& operator<<(ostream& in, BinarySearchTree<S>& obj);
+
+	template<class S>
+	friend ostream& inorderPrint(ostream& out, BTNode<S>* node);
+
 protected:
-  // helper functions
-  //BTNode<T>* root;
+	// helper functions
+	//BTNode<T>* root;
 };
 
 #include "BinarySearchTree.cpp"
--- test_bst.cc
+++ test_bst.cc
@@ -1,6 +1,5 @@
-#include "BinaryTree.h"
 #include "BinarySearchTree.h"
-
+#include "BinaryTree.h"
 
 using namespace std;
 
@@ -8,48 +7,36 @@ using namespace std;
 /* Main */
 /********/
 
-int main( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
-  BinarySearchTree<string> bst;
+	// create a new tree from an string input stream using a fixed phrase
+	istringstream instr("What fresh hell could this be?");
+	BinarySearchTree<string> bst;
+	instr >> bst;
+	cout << bst << "\n";
+	int k[8] = { 1,2,3,4,5,6,7,8 };
+	BinarySearchTree<int> intTree;
+	intTree.init_from_sorted_array(k, 8);
+	PDF *pdf = new PDF("intTree.pdf");
+	intTree.display(pdf, "Int Tree");
+	pdf->finish();
+	delete pdf;
+	int l[8] = { 4, 1, 6, 7, 3, 5, 10, 2 };
+	intTree.init(l, 8);
+	pdf = new PDF("intTree2.pdf");
+	intTree.display(pdf, "Int Tree");
+	pdf->finish();
+	delete pdf;
+	int* myInt = intTree.lookup(7);
+	cout << "My int = " << *myInt << " should be 7" << endl;
+	int my[8] = { 0 };
+	int my2[8] = { 1, 2, 3, 5, 6, 7, 10 };
+	intTree.to_array(my, 7);
+	for (int anInt = 0; anInt < 7; anInt++)
+		cout << my[anInt] << " ," << my2[anInt] << endl;
+	/* The output should be
 
-  // initialize 'bst' from the standard input
-  cin >> bst;
+	   What be? could fresh hell this
 
-  // write it as a PDF file
-  PDF *pdf = new PDF("bst.pdf");
-  bst.display(pdf, "My binary Tree");
-  pdf->finish();
-  delete pdf;
-  cout << bst << endl;
-  // create a new tree from an string input stream using a fixed phrase
-  istringstream instr("What fresh hell could this be?");
-  BinarySearchTree<string> bst2;
-  instr >> bst2;
-  cout << bst2 << "\n";
-  int k[8] = {1,2,3,4,5,6,7,8};
-  BinarySearchTree<int> intTree;
-  intTree.init_from_sorted_array(k, 8);
-  pdf = new PDF("intTree.pdf");
-  intTree.display(pdf, "Int Tree");
-  pdf->finish();
-  delete pdf;
-  int l[8] = {4, 1, 6, 7, 3, 5, 10, 2};
-  intTree.init(l, 8);
-  pdf = new PDF("intTree2.pdf");
-  intTree.display(pdf, "Int Tree");
-  pdf->finish();
-  delete pdf;
-  int* myInt = intTree.lookup( 7 );
-  cout << "My int = " << *myInt << " should be 7" << endl;
-  int my[8] = {0};
-  int my2[8] = {1, 2, 3, 5, 6, 7, 10}; 
-  intTree.to_array(my, 7);
-  bool ak;
-  for( int anInt = 0; anInt < 7; anInt++ )
-       cout << my[anInt] << " ," << my2[anInt] << endl;
-  /* The output should be
-     
-     What be? could fresh hell this
-
-  */
+	*/
 }
